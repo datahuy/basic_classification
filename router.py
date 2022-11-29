bbef678930d6f0bba6a4908d667739fb9dfdb1c9
Lê Đức Bảo - Falcon <bao.ld1@citigo.com.vn>
2022-11-29 18:22:47 +0700

    add FMCG classifier

--- router.py
+++ router.py
@@ -7,6 +7,7 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 from main.classifier.industry_classifier import IndustryClassifier
+from main.classifier.FMCG_classifier import FMCGClassifier
 
 logging.basicConfig(
         level=logging.INFO,
@@ -19,6 +20,7 @@ logging.basicConfig(
 
 # Define classification models
 industry_classifier = IndustryClassifier('model/industry_cls_best_1129.pt', batch_size=128)
+FMCG_classifier = FMCGClassifier('model/model_fmcg_binary_best_converted.pt', batch_size=128)
 
 class Body(BaseModel):
     product_name: Union[str, list]
@@ -47,4 +49,27 @@ def industry_cls(body_params: Body):
             "status": "error",
             "status_code": 500,
             "message": f"Exception in industry_cls: {e}"
+        }
+
+@app.post("/FMCG_cls/")
+def FMCG_cls(body_params: Body):
+    product_name, threshold = body_params.product_name, body_params.threshold
+    if type(product_name) == str:
+        product_name = [product_name]
+    try:
+        preds, probs = FMCG_classifier.predict(product_name, threshold)
+        result = [{"product_name": n, "industry": p, "score": str(s)} for n, p, s in zip(product_name, preds, probs)]
+        return {
+            "data": result,
+            "status": "success",
+            "status_code": 200,
+            "message": "Classify FMCG done"
+        }
+    except Exception as e:
+        logging.error(e)
+        return {
+            "data": product_name,
+            "status": "error",
+            "status_code": 500,
+            "message": f"Exception in FMCG_cls: {e}"
         }
