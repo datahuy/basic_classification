b068bc245dab3883971652dedfcc926d7b0f1549
Ngô Phương Nhi - Data Department <nhi.np@citigo.com.vn>
2022-11-30 11:59:20 +0700

    debug router

--- router.py
+++ router.py
@@ -26,25 +26,30 @@ class Body(BaseModel):
 
 app = FastAPI()
 
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
 @app.post("/industry_cls/")
 def industry_cls(body_params: Body):
+    print(body_params)
     product_name, threshold = body_params.product_name, body_params.threshold
     if type(product_name) == str:
         product_name = [product_name]
-    try:
-        preds, probs = industry_classifier.predict(product_name, threshold)
-        result = [{"product_name": n, "industry": p, "score": str(s)} for n, p, s in zip(product_name, preds, probs)]
-        return {
-            "data": result,
-            "status": "success",
-            "status_code": 200,
-            "message": "Classify industry level done"
-        }
-    except Exception as e:
-        logging.error(e)
-        return {
-            "data": product_name,
-            "status": "error",
-            "status_code": 500,
-            "message": f"Exception in industry_cls: {e}"
-        }
+    # try:
+    preds, probs = industry_classifier.predict(product_name, threshold)
+    result = [{"product_name": n, "industry": p, "score": str(s)} for n, p, s in zip(product_name, preds, probs)]
+    return {
+        "data": result,
+        "status": "success",
+        "status_code": 200,
+        "message": "Classify industry level done"
+    }
+    # except Exception as e:
+    #     logging.error(e)
+    #     return {
+    #         "data": product_name,
+    #         "status": "error",
+    #         "status_code": 500,
+    #         "message": f"Exception in industry_cls: {e}"
+    #     }
