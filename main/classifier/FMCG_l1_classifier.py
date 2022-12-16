e449e64aa37b571181d4ed3b59f31b07b2cf9259
Lê Đức Bảo - Falcon <bao.ld1@citigo.com.vn>
2022-12-16 11:44:10 +0700

    rename file

--- main/classifier/FMCG_l1_classifier.py
+++ main/classifier/FMCG_l1_classifier.py
@@ -2,11 +2,11 @@ from main.classifier.base_classifier import BaseClassifier
 
 
 class FMCGl1Classifier(BaseClassifier):
-    def __init__(self, model_path, batch_size):
+    def __init__(self, model_path, batch_size=128):
         name = 'FMCG l1 Classifier'
         super(FMCGl1Classifier, self).__init__(model_path, batch_size, name)
 
-    def predict(self, input, batch_size):
-        preds, probs = self.model.predict(input, batch_size)
+    def predict(self, input):
+        preds, probs = self.model.predict(input, batch_size=self.batch_size)
         #output = self.postprocess(preds, probs, threshold)
         return preds
