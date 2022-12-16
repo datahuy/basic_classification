b9d84611400d35fec86bded53bebed12a1f1a227
Lê Đức Bảo - Falcon <bao.ld1@citigo.com.vn>
2022-12-16 17:27:30 +0700

    change mapping to regex

--- main/rule/rule_mapping.py
+++ main/rule/rule_mapping.py
@@ -1,43 +1,56 @@
 import json
 from typing import List
-
-import pandas as pd
-import os
 from tqdm import tqdm
+import re
 
 
 def read_json(js):
-    f = open(js)
-    keyword = json.load(f)
+    with open(js, 'rb') as f:
+        keyword = json.load(f)
     return keyword
 
-
-def read_jl(jl_file):
-    with open(jl_file, encoding="utf-8") as file:
-        data = list(file)
-    lst_jl = [json.loads(json_str) for json_str in data]
-    return lst_jl
+def reverse_dict(d):
+    reversed_dict = {}
+    for key, value in d.items():
+        for v in value:
+            reversed_dict[v] = key
+    return reversed_dict
 
 
 def mapping(string, keywords):
-    result = [key for key, value in keywords.items() if any(val in string for val in value)]
-    if not result:
-        result.append("unk")
-    return result[0]
+    '''
+    string: input string
+    keywords: dictionary in the form of {keyword: category}
 
+    this function will build a regex pattern from the list of keywords, then match the string with the pattern.
+    The regex pattern is from all category.
+    The regex is sorted by the length of the keyword in descending order.
+    If there is a match, the function will return the category of the string.
+    '''
+    string = string.lower()
+    keyword_list = sorted(keywords.keys(), key=lambda x: len(x), reverse=True)
+    pattern = f'(?<=\\b)({"|".join(keyword_list)})(?=\\b)'
+    pattern = re.compile(pattern)
+    match = pattern.search(string)
+    if match:
+        return keywords[match.group()]
+    else:
+        return 'unk'
+        
 
 def rule_predict_batch(input: List, batch_size, keywords):
-    preds = []
-    if len(input) >= batch_size:
-        for i in range(0, len(input), batch_size):
-            pred = [mapping(j.lower(), keywords=keywords) for j in input[i:batch_size]]
-            preds.extend(pred)
-    else:
-        preds = [mapping(j.lower(), keywords=keywords) for j in input]
-    return preds
+    output = []
+    for i in tqdm(range(0, len(input), batch_size)):
+        input_batch = input[i:i + batch_size]
+        input_batch = [x.lower() for x in input_batch]
+        output_batch = [mapping(x, keywords) for x in input_batch]
+        output.extend(output_batch)
+    return output
 
 
 if __name__ == '__main__':
     jl = read_json("main/rule/data-bin/keyword_lv1.json")
+    reversed_jl = reverse_dict(jl)
     test = ["gạo nếp 100kg", "gạo tẻ"]
-    print(mapping(test[0], jl))
+    for product in test:
+        print(mapping(product, reversed_jl))
