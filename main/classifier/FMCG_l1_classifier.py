9375a6098e6d4ce0674dc957c1d74819f1dc317d
Lê Đức Bảo - Falcon <bao.ld1@citigo.com.vn>
2022-12-16 17:46:18 +0700

    add rule classifier class

--- main/classifier/FMCG_l1_classifier.py
+++ main/classifier/FMCG_l1_classifier.py
@@ -1,4 +1,16 @@
 from main.classifier.base_classifier import BaseClassifier
+from main.rule.rule_mapping import read_json, rule_predict_batch, reverse_dict, build_regex
+import logging
+import sys 
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - {%(pathname)s:%(lineno)d} - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
 
 
 class FMCGl1Classifier(BaseClassifier):
@@ -10,3 +22,15 @@ class FMCGl1Classifier(BaseClassifier):
         preds, probs = self.model.predict(input, batch_size=self.batch_size)
         #output = self.postprocess(preds, probs, threshold)
         return preds
+
+
+class FMCGl1RuleClassifier():
+    def __init__(self, json_path, batch_size=128):
+        self.keywords = reverse_dict(read_json(json_path))
+        self.pattern = build_regex(self.keywords)
+        self.batch_size = batch_size
+        logging.info('Finish loading FMCG l1 rule classifier')
+
+    def predict(self, input):
+        output = rule_predict_batch(input, keywords=self.keywords, pattern=self.pattern, batch_size=self.batch_size)
+        return output
