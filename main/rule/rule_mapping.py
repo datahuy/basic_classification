617f3802a62d32edd048d4a93ca4fc6fb1f6920c
Lê Đức Bảo - Falcon <bao.ld1@citigo.com.vn>
2022-12-16 17:46:50 +0700

    refactor mapping function

--- main/rule/rule_mapping.py
+++ main/rule/rule_mapping.py
@@ -17,20 +17,15 @@ def reverse_dict(d):
     return reversed_dict
 
 
-def mapping(string, keywords):
-    '''
-    string: input string
-    keywords: dictionary in the form of {keyword: category}
-
-    this function will build a regex pattern from the list of keywords, then match the string with the pattern.
-    The regex pattern is from all category.
-    The regex is sorted by the length of the keyword in descending order.
-    If there is a match, the function will return the category of the string.
-    '''
-    string = string.lower()
+def build_regex(keywords):
     keyword_list = sorted(keywords.keys(), key=lambda x: len(x), reverse=True)
     pattern = f'(?<=\\b)({"|".join(keyword_list)})(?=\\b)'
     pattern = re.compile(pattern)
+    return pattern
+
+
+def mapping(string, keywords, pattern):
+    string = string.lower()
     match = pattern.search(string)
     if match:
         return keywords[match.group()]
@@ -38,12 +33,12 @@ def mapping(string, keywords):
         return 'unk'
         
 
-def rule_predict_batch(input: List, batch_size, keywords):
+def rule_predict_batch(input: List, batch_size, keywords, pattern):
     output = []
     for i in tqdm(range(0, len(input), batch_size)):
         input_batch = input[i:i + batch_size]
         input_batch = [x.lower() for x in input_batch]
-        output_batch = [mapping(x, keywords) for x in input_batch]
+        output_batch = [mapping(x, keywords, pattern) for x in input_batch]
         output.extend(output_batch)
     return output
 
@@ -51,6 +46,7 @@ def rule_predict_batch(input: List, batch_size, keywords):
 if __name__ == '__main__':
     jl = read_json("main/rule/data-bin/keyword_lv1.json")
     reversed_jl = reverse_dict(jl)
-    test = ["gạo nếp 100kg", "gạo tẻ"]
+    pattern = build_regex(reversed_jl)
+    test = ["sữa đặc", "sữa chua vinamilk không đường", "sữa rửa mặt nivea", "thuốc lá thăng long", "srm adg"]
     for product in test:
-        print(mapping(product, reversed_jl))
+        print(mapping(product, reversed_jl, pattern))
