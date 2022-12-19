3fcfcd8724a8e49bc0fe11c324078ba449d68505
Lê Đức Bảo - Falcon <bao.ld1@citigo.com.vn>
2022-12-19 01:56:24 +0000

    Baold

--- main/utils/pipeline.py
+++ main/utils/pipeline.py
@@ -3,7 +3,11 @@ from main.rule.rule_mapping import rule_predict_batch
 
 
 def merge_output(model_output: List, rule_output: List):
-    indexes = [idx for idx, j in enumerate(rule_output) if j != "unk"]
-    for (index, r_out) in zip(indexes, rule_output):
-        model_output[index] = r_out
-    return model_output
+    # if rule_output is unk, use model output, else use rule output
+    merged = []
+    for model, rule in zip(model_output, rule_output):
+        if rule == "unk":
+            merged.append(model)
+        else:
+            merged.append(rule)
+    return merged
