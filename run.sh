5ef7b8c210b90936344e97f28a0e37281d15910b
Lê Đức Bảo - Falcon <bao.ld1@citigo.com.vn>
2022-12-20 17:38:22 +0700

    add reload option

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 gunicorn router:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:9201
-# uvicorn router:app --workers 4 --host 127.0.0.1 --port 9201 --reload
+# uvicorn router:app --workers 4 --host 0.0.0.0 --port 9201
